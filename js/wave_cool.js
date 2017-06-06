6137c37dd5c2e775f9c7abbe3734e7f03e71301e
CoDeleven <codelevex@gmail.com>
2017-06-06 09:21:31 +0800

    将整体参数移出来，并添加 加载速度，如果加载速度不为0，那么会慢慢上涨

--- js/wave_cool.js
+++ js/wave_cool.js
@@ -1,43 +1,70 @@
 var ctx;
+var curSpeed = 0;
+var accSpeed = 0.5;
+// 绘制点的总数
+var maxPoint = 100;
+// 每100个点一个周期, maxPoint / piCycl 可以获取波动的sin函数的个数
+var piCycl = 25;
+// x轴放大的倍数
+var xMultiple = 15;
+// y轴放大的倍数
+var yMultiple = 6;
+// y轴起始偏移量
+var yOffset = 45;
+// y轴加载的速度
+var yLoadSpeed = 0;
+// 波浪颜色
+var waveColor = "rgba(52, 152, 219,1.0)";
+// 描绘波浪边的颜色
+var waveLineColor = "rgba(44, 62, 80,0.4)";
+// 描绘波浪阴影的大小
+var waveLineShadowBlur = 2;
+// 波浪边上的阴影
+var waveLineShadowColor = "rgba(44, 62, 80,0.4)";
+// 中间字体的样式
+var fontStyle = "50px Georgia";
+// 要显示的字
+var core = "帅";
 window.onload = function() {
 	var canvas = document.getElementById("myCanvas");
 	ctx = canvas.getContext("2d");
 	ctx.moveTo(0, 100);
-	var j = 0.1;
 	setInterval(function() {
-		j += 0.5;
+		curSpeed += accSpeed;
 
-		ctx.clearRect(0, 0, 500, 300);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.beginPath();
-		ctx.lineTo(0, 300);
+		ctx.lineTo(-30, canvas.height);
 
-		for(var i = 0; i < 400; ++i) {
-			var x = (i / 100) * Math.PI;
+		for(var i = 0; i < maxPoint; ++i) {
+			// 计算当前point在一个周期内的位置，并根据位置计算sin值
+			var x = (i / piCycl) * Math.PI;
 			// 放大x轴10倍，然后根据sin(x)计算出y轴并放大7倍 - 45个像素;
-			ctx.lineTo(x * 10, Math.sin(x + j) * 6 + 45);
+			ctx.lineTo(x * xMultiple, Math.sin(x + curSpeed) * yMultiple + yOffset);
+
 		}
 
 		ctx.lineTo(x * 10, 300);
-		ctx.fillStyle = "rgba(52, 152, 219,1.0)";
+		ctx.fillStyle = waveColor;
 		ctx.fill();
 		// 保存波浪效果去堆栈1
 		ctx.save();
 
 		// 设置取新图片和原图的交集，保留交集图片 图1
 		ctx.globalCompositeOperation = "source-in";
-		ctx.font = "50px Georgia";
+		ctx.font = fontStyle;
 		ctx.fillStyle = "white";
 		// 此时为图1
-		ctx.fillText("帅", 25, 60);
+		ctx.fillText(core, 25, 60);
 
 		// 取出堆栈1的效果即波浪效果
 		ctx.restore();
 		// 将后续的模糊效果添加至图1，将新效果添加到图1上面
 		ctx.globalCompositeOperation = "destination-over";
-		ctx.strokeStyle = "rgba(44, 62, 80,0.4)";
-		ctx.lineWidth = 5;
-		ctx.shadowBlur = 4;
-		ctx.shadowColor = "rgba(44, 62, 80,0.6)";
+		ctx.strokeStyle = "";
+		ctx.lineWidth = 3;
+		ctx.shadowBlur = waveLineShadowBlur;
+		ctx.shadowColor = "";
 		// 此时为图2
 		ctx.stroke();
 
@@ -46,17 +73,20 @@ window.onload = function() {
 		// 重置阴影属性
 		ctx.shadowBlur = 0;
 		ctx.shadowOffsetX = 0;
-		ctx.fillStyle = "rgba(52, 152, 219,1.0)";
+		ctx.fillStyle = waveColor;
 		// 此时为图3
 		ctx.fill();
 
 		// 将后续的文字绘制到图3上
 		ctx.globalCompositeOperation = "destination-over";
-		ctx.font = "50px Georgia";
-		ctx.fillStyle = "rgba(52, 152, 219,1.0)";
+		ctx.font = fontStyle;
+		ctx.fillStyle = waveColor;
 		// 此时为图4
-		ctx.fillText("帅", 25, 60);
-
+		ctx.fillText(core, 25, 60);
+		
+		if(yLoadSpeed > 0 && yOffset > 0) {
+			yOffset -= yLoadSpeed;
+		}
 	}, 40);
 
 };
