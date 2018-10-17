f648c4f84301be6baa0e0b7ef0d36790e68241c7
codeleven <codelevex@gmail.com>
2018-10-17 22:16:21 +0800

    完整的内容

--- js/heart_wave_cool.js
+++ js/heart_wave_cool.js
@@ -1,4 +1,3 @@
-var ctx;
 var curSpeed = 0;
 var accSpeed = 0.5;
 // 绘制点的总数
@@ -6,9 +5,9 @@ var maxPoint = 300;
 // 每100个点一个周期, maxPoint / piCycl 可以获取波动的sin函数的个数
 var piCycl = 25;
 // x轴放大的倍数
-var xMultiple = 30;
+var xMultiple = 55;
 // y轴放大的倍数
-var yMultiple = 11;
+var yMultiple = 12;
 // y轴起始偏移量
 var yOffset = 220;
 // y轴加载的速度
@@ -22,14 +21,24 @@ var waveLineShadowBlur = 2;
 // 波浪边上的阴影
 var waveLineShadowColor = "rgba(44, 62, 80,0.4)";
 // 中间字体的样式
-var fontStyle = "20px Georgia";
+var fontStyle = "75px SimHei";
+var fontStyle2 = "55px SimHei";
 // 要显示的字
-var core = "瘟猪和骚鹿在一起的";
-var core1 = "第{DAY}天"
-var core2 = "{hh}时{MM}分{ss}秒"
+var core = "瘟猪和骚鹿在一起";
+var core1 = "第 137 天";
+var core2 = "{hh}时{MM}分{ss}秒";
+
+const heartXStart = 510;
+const heartYStart = 440;
+const text1XStart = 510;
+const text1YStart = 400;
+const text2XStart = 525;
+const text2YStart = 520;
+const text3XStart = 525;
+const text3YStart = 600;
 window.onload = function () {
 	var canvas = document.getElementById("myCanvas");
-	ctx = canvas.getContext("2d");
+	var ctx = canvas.getContext("2d");
 
 	var canvas2 = document.getElementById("myCanvas2");
 	var ctx2 = canvas2.getContext("2d");
@@ -55,11 +64,13 @@ window.onload = function () {
 function drawCanvas3(ctx3, canvas3) {
 	ctx3.beginPath();
 	ctx3.strokeStyle = waveColor;
+	ctx3.lineWidth = 5;
+	ctx3.lineJoin = "round";
 	for (var i = 0; i <= Math.PI * 3; i += 0.02) {
 		var x = getHeartX(i);
 		var y = getHeartY(i);
 		// 因为y是增量，由于x，y是第四象限，所以要减去y
-		ctx3.lineTo(200 + x, 200 - y);
+		ctx3.lineTo(heartXStart + x, heartYStart - y);
 	}
 	ctx3.stroke();
 }
@@ -72,9 +83,10 @@ function drawCanvas(ctx, canvas) {
 	ctx.fillStyle = "#FFFFFF";
 	ctx.textAlign = "center";
 	// 此时为图1
-	ctx.fillText(core, 200, 170);
-	ctx.fillText(core1, 200, 220);
-	ctx.fillText(core2, 200, 250);
+	ctx.fillText(core, text1XStart, text1YStart);
+	ctx.font = fontStyle2;
+	ctx.fillText(core1, text2XStart, text2YStart);
+	ctx.fillText(core2, text3XStart, text3YStart);
 
 	ctx.globalCompositeOperation = "destination-in";
 	drawWave(ctx, canvas);
@@ -89,35 +101,26 @@ function drawCanvas(ctx, canvas) {
 		var x = getHeartX(i);
 		var y = getHeartY(i);
 		// 因为y是增量，由于x，y是第四象限，所以要减去y
-		ctx.lineTo(200 + x, 200 - y);
+		ctx.lineTo(heartXStart + x, heartYStart - y);
 	}
 	ctx.fill();
 }
 
 function drawCanvas2(ctx2, canvas2) {
 	ctx2.globalCompositeOperation = "source-over";
-	ctx2.font = "20px Georgia";
+	ctx2.font = fontStyle;
 	ctx2.fillStyle = "#000000";
 	ctx2.textAlign = "center";
-	ctx2.fillText("瘟猪和骚鹿在一起的", 200, 170);
-	ctx2.fillText("第{DAY}天", 200, 220);
-	ctx2.fillText("{hh}时{MM}分{ss}秒", 200, 250);
+	ctx2.fillText(core, text1XStart, text1YStart);
+	ctx2.font = fontStyle2;
+	ctx2.fillText(core1, text2XStart, text2YStart);
+	ctx2.fillText(core2, text3XStart, text3YStart);
 
 	ctx2.globalCompositeOperation = "destination-out";
 	drawWave(ctx2, canvas2);
 }
 
-function drawCustomBorder(ctx, canvas) {
-	ctx.globalCompositeOperation = "destination-in";
-
-	ctx.restore();
-	ctx.save();
-
-
-	// ctx.restore();
-}
-
-const magnitude = 10;
+const magnitude = 30;
 
 /**
  * 绘制波浪效果，最后会保存到到层1里
@@ -133,34 +136,11 @@ function drawWave(ctx, canvas) {
 		var x = (i / piCycl) * Math.PI;
 		// 放大x轴10倍，然后根据sin(x)计算出y轴并放大7倍 - 45个像素;
 		ctx.lineTo(x * xMultiple, Math.sin(x + curSpeed) * yMultiple + yOffset);
-
 	}
 
-	ctx.lineTo(x * 10, 500);
+	ctx.lineTo(x * 25, 900);
 	ctx.fillStyle = waveColor;
 	ctx.fill();
-}
-
-
-
-function drawText(ctx, canvas) {
-	ctx.save();
-	ctx.globalCompositeOperation = "source-in";
-
-	// 设置取新图片和原图的交集，保留交集图片 图1
-	ctx.font = fontStyle;
-	ctx.fillStyle = "#000000";
-	ctx.textAlign = "center";
-	// 此时为图1
-	ctx.fillText(core, 200, 170);
-	// ctx.fillText(core1, 200, 220);
-	// ctx.fillText(core2, 200, 250);
-
-	// ctx.restore();
-	// ctx.stroke();
-	// ctx.fill();
-
-
 }
 
 
